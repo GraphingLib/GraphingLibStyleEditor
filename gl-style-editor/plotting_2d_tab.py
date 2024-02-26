b684e9e34edc03430755403ebba71f631540206f
Gustave Coulombe <magikgus@gmail.com>
2024-02-25 23:46:11 -0500

    Filled in Contour tab

--- gl-style-editor/plotting_2d_tab.py
+++ gl-style-editor/plotting_2d_tab.py
@@ -1,13 +1,33 @@
-from PyQt5.QtWidgets import QVBoxLayout, QTabWidget, QWidget, QMainWindow
+from matplotlib import colormaps
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QFrame,
+    QLabel,
+    QMainWindow,
+    QScrollArea,
+    QTabWidget,
+    QVBoxLayout,
+    QWidget,
+)
+
+from .widgets import CheckBox, ListOptions, Slider
 
 
 def create_plotting_2d_tab(window: QMainWindow):
     layout = QVBoxLayout()
     tabWidget = QTabWidget()
 
-    # Example stubs for each sub-tab
+    # contour tab
+    contourTabLayout = create_contour_tab(window)
     contourTab = QWidget()
-    tabWidget.addTab(contourTab, "Contour")
+    contourTab.setLayout(contourTabLayout)
+    contourTabScrollArea = QScrollArea()
+    contourTabScrollArea.setWidgetResizable(True)
+    contourTabScrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+    contourTabScrollArea.setWidget(contourTab)
+    tabWidget.addTab(contourTabScrollArea, "Contour")
+
+    # Example stubs for each sub-tab
     heatmapTab = QWidget()
     tabWidget.addTab(heatmapTab, "Heatmap")
     streamTab = QWidget()
@@ -17,3 +37,60 @@ def create_plotting_2d_tab(window: QMainWindow):
 
     layout.addWidget(tabWidget)
     window.plotting2DTab.setLayout(layout)
+
+
+def create_contour_tab(window: QMainWindow):
+    # Create a layout for the contour tab
+    layout = QVBoxLayout()
+    layout.setAlignment(Qt.AlignTop)
+
+    # create colormap dropdown
+    colormap = ListOptions(
+        window,
+        "Colormap",
+        list(colormaps),
+        ["Contour", "color_map"],
+    )
+    layout.addWidget(colormap)
+
+    # create number_of_levels slider
+    levels = Slider(
+        window,
+        "Number of Levels",
+        1,
+        50,
+        2,
+        ["Contour", "number_of_levels"],
+        conversion_factor=1,
+    )
+    layout.addWidget(levels)
+
+    # create alpha slider
+    alpha = Slider(
+        window,
+        "Opacity",
+        0,
+        100,
+        5,
+        ["Contour", "alpha"],
+        conversion_factor=100,
+    )
+    layout.addWidget(alpha)
+
+    # create show_color_bar checkbox
+    show_colorbar = CheckBox(
+        window,
+        "Show Colorbar",
+        ["Contour", "show_color_bar"],
+    )
+    layout.addWidget(show_colorbar)
+
+    # create filled checkbox
+    filled = CheckBox(
+        window,
+        "Filled",
+        ["Contour", "filled"],
+    )
+    layout.addWidget(filled)
+
+    return layout
