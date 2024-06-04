bac42f3532a5ab654caef7cf36495b7349b8392e
Gustave Coulombe <magikgus@gmail.com>
2024-06-04 14:59:10 -0400

    Added limits to line plot

--- glse/figures/line.py
+++ glse/figures/line.py
@@ -5,6 +5,6 @@ line2 = gl.Line(pointA=(0, 1), pointB=(1, 0), capped_line=True)
 line3 = gl.Line(pointA=(0, 2), pointB=(1, 3))
 line4 = gl.Line(pointA=(0, 3), pointB=(1, 2), capped_line=True)
 
-fig = gl.Figure()
+fig = gl.Figure(x_lim=(-1, 2), y_lim=(-1, 4))
 fig.add_elements(line, line2, line3, line4)
 fig.show()
