8568ebdf6e098b985c942b5d87db5c0723500a63
Gustave Coulombe <magikgus@gmail.com>
2024-06-15 18:13:14 -0400

    Changed heatmap example figure

--- glse/figures/heatmap.py
+++ glse/figures/heatmap.py
@@ -3,7 +3,7 @@ import graphinglib as gl
 # Create heatmap
 
 heatmap = gl.Heatmap.from_function(
-    lambda x, y: x**2 + y**2, x_axis_range=(-10, 10), y_axis_range=(-10, 10)
+    lambda x, y: x**2 + y**2, x_axis_range=(0, 10), y_axis_range=(0, 10)
 )
 
 fig = gl.Figure()
