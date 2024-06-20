21e216cf25f9742d8b5a126c140945e8e21f67b9
Gustave Coulombe <magikgus@gmail.com>
2024-06-20 16:19:51 -0400

    Added more customization options in figure tab

--- glse/figure_tab.py
+++ glse/figure_tab.py
@@ -1,22 +1,22 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QCheckBox, QFrame, QLabel, QMainWindow, QVBoxLayout
+from PySide6.QtWidgets import QFrame, QLabel, QMainWindow, QVBoxLayout
 
-from .widgets import CheckBox, ColorPickerWidget, Dropdown, Slider
+from .widgets import Activator, CheckBox, ColorPickerWidget, Dropdown, Slider
 
 
 def create_figure_tab(window: QMainWindow):
     figureTabLayout = QVBoxLayout()
     figureTabLayout.setAlignment(Qt.AlignTop)
 
-    # Section for Colors
-    colors_label = QLabel("Colors:")
-    colors_label.setStyleSheet("font-weight: bold;")
-    figureTabLayout.addWidget(colors_label)
+    # Section for general figure settings
+    figure_label = QLabel("General Figure Settings:")
+    figure_label.setStyleSheet("font-weight: bold;")
+    figureTabLayout.addWidget(figure_label)
 
-    colors_line = QFrame()
-    colors_line.setFrameShape(QFrame.HLine)
-    colors_line.setFrameShadow(QFrame.Sunken)
-    figureTabLayout.addWidget(colors_line)
+    figure_line = QFrame()
+    figure_line.setFrameShape(QFrame.HLine)
+    figure_line.setFrameShadow(QFrame.Sunken)
+    figureTabLayout.addWidget(figure_line)
 
     # figure face color
     figure_color_widget = ColorPickerWidget(
@@ -45,6 +45,31 @@ def create_figure_tab(window: QMainWindow):
     )
     figureTabLayout.addWidget(axes_edge_color_widget)
 
+    # axes label color
+    axes_label_color_widget = ColorPickerWidget(
+        window,
+        label="Axis Labels Color:",
+        param_ids=["rc_params", ["axes.labelcolor"]],
+        initial_color=window.params["rc_params"]["axes.labelcolor"],
+    )
+    figureTabLayout.addWidget(axes_label_color_widget)
+
+    # axes line width
+    axes_line_width_slider = Slider(
+        window, "Axes Line Width:", 0, 20, 1, ["rc_params", "axes.linewidth"]
+    )
+    figureTabLayout.addWidget(axes_line_width_slider)
+
+    # Section for axes ticks
+    ticks_label = QLabel("Ticks:")
+    ticks_label.setStyleSheet("font-weight: bold;")
+    figureTabLayout.addWidget(ticks_label)
+
+    ticks_line = QFrame()
+    ticks_line.setFrameShape(QFrame.HLine)
+    ticks_line.setFrameShadow(QFrame.Sunken)
+    figureTabLayout.addWidget(ticks_line)
+
     # ticks color
     axes_ticks_color_widget = ColorPickerWidget(
         window,
@@ -53,6 +78,57 @@ def create_figure_tab(window: QMainWindow):
         initial_color=window.params["rc_params"]["xtick.color"],
     )
     figureTabLayout.addWidget(axes_ticks_color_widget)
+
+    # ticks direction
+    ticks_direction_dropdown = Dropdown(
+        window,
+        "Ticks Direction:",
+        ["In", "Out", "In/Out"],
+        ["in", "out", "inout"],
+        ["rc_params", ["xtick.direction", "ytick.direction"]],
+    )
+    figureTabLayout.addWidget(ticks_direction_dropdown)
+
+    # Section for legend
+    legend_label = QLabel("Legend:")
+    legend_label.setStyleSheet("font-weight: bold;")
+    figureTabLayout.addWidget(legend_label)
+
+    legend_line = QFrame()
+    legend_line.setFrameShape(QFrame.HLine)
+    legend_line.setFrameShadow(QFrame.Sunken)
+    figureTabLayout.addWidget(legend_line)
+
+    # legend face color
+    legend_face_color_widget = ColorPickerWidget(
+        window,
+        label="Legend Face Color:",
+        param_ids=["rc_params", ["legend.facecolor"]],
+        initial_color=window.params["rc_params"]["legend.facecolor"],
+    )
+    figureTabLayout.addWidget(legend_face_color_widget)
+
+    # legend edge color
+    legend_edge_color_widget = ColorPickerWidget(
+        window,
+        label="Legend Edge Color:",
+        param_ids=["rc_params", ["legend.edgecolor"]],
+        initial_color=(
+            window.params["rc_params"]["legend.edgecolor"]
+            if window.params["rc_params"]["legend.edgecolor"] != "none"
+            else "#000000"
+        ),
+    )
+
+    legend_edge_color_activator = Activator(
+        window,
+        legend_edge_color_widget,
+        param_ids=["rc_params", ["legend.edgecolor"]],
+        check_label="None",
+        param_if_checked="none",
+    )
+
+    figureTabLayout.addWidget(legend_edge_color_activator)
 
     # Section for Grid
     grid_label = QLabel("Grid:")
@@ -77,6 +153,18 @@ def create_figure_tab(window: QMainWindow):
     )
     figureTabLayout.addWidget(axes_grid_color_widget)
 
+    # grid alpha
+    axes_grid_alpha_slider = Slider(
+        window,
+        "Grid Opacity:",
+        0,
+        10,
+        1,
+        ["rc_params", "grid.alpha"],
+        conversion_factor=10,
+    )
+    figureTabLayout.addWidget(axes_grid_alpha_slider)
+
     # grid line width (use slider)
     grid_line_width_slider = Slider(
         window, "Grid Line Width:", 0, 20, 1, ["rc_params", "grid.linewidth"]
@@ -92,6 +180,42 @@ def create_figure_tab(window: QMainWindow):
         ["rc_params", "grid.linestyle"],
     )
     figureTabLayout.addWidget(grid_line_style_dropdown)
+
+    # Text Section
+    text_label = QLabel("Text:")
+    text_label.setStyleSheet("font-weight: bold;")
+    figureTabLayout.addWidget(text_label)
+
+    text_line = QFrame()
+    text_line.setFrameShape(QFrame.HLine)
+    text_line.setFrameShadow(QFrame.Sunken)
+    figureTabLayout.addWidget(text_line)
+
+    # font family
+    font_family_dropdown = Dropdown(
+        window,
+        "Font Family:",
+        ["Sans-serif", "Serif", "Monospace"],
+        ["sans-serif", "serif", "monospace"],
+        ["rc_params", "font.family"],
+    )
+    figureTabLayout.addWidget(font_family_dropdown)
+
+    # font size
+    font_size_slider = Slider(
+        window, "Font Size:", 0, 30, 1, ["rc_params", "font.size"]
+    )
+    figureTabLayout.addWidget(font_size_slider)
+
+    # font weight (doesn't work with title and axis labels)
+    # font_weight_dropdown = Dropdown(
+    #     window,
+    #     "Font Weight:",
+    #     ["Normal", "Bold"],
+    #     ["normal", "bold"],
+    #     ["rc_params", "font.weight"],
+    # )
+    # figureTabLayout.addWidget(font_weight_dropdown)
 
     return figureTabLayout
 
